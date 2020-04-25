ed531c58929d626988445a6a1cff898a09e5edc6
粒粒橙 <i@llc.moe>
2020-04-25 13:37:48 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      }
+    ]
+  }
+}
