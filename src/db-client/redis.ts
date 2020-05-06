cec00e397dcf6dbd6c16afcec47dee176f81b4c9
粒粒橙 <i@llc.moe>
2020-05-07 00:30:17 +0800

    redis pipeline and scripts

--- src/db-client/redis.ts
+++ src/db-client/redis.ts
@@ -1,4 +1,3 @@
-import { createHash } from 'crypto'
 import { Redis } from 'ioredis'
 import { ECommand } from '../types'
 import { DBClient } from './db'
@@ -21,12 +20,15 @@ class RedisClient extends DBClient {
   public listAccounts = async (): Promise<ListResult> => {
     try {
       const accounts = await this.cl.keys('user:*')
-      const result = await Promise.all(
-        accounts.map(async (user: string) => ({
-          id: parseInt(user.slice(5), 10),
-          password: (await this.cl.get(user)) || '',
-        })),
-      )
+      let pipe = this.cl.pipeline()
+      accounts.forEach((acct) => {
+        pipe = pipe.get(acct)
+      })
+      const result = (await pipe.exec()).map((item, idx) => ({
+        id: parseInt(accounts[idx].slice(5), 10),
+        password: item[1] || '',
+      }))
+      logger.debug('List: ' + JSON.stringify(result))
       return { type: ECommand.List, data: result }
     } catch (e) {
       throw new Error("Query error on 'list': " + e.message)
@@ -37,23 +39,32 @@ class RedisClient extends DBClient {
     acctId: number,
     password: string,
   ): Promise<AddResult> => {
-    const key = createHash('sha224')
-      .update(`${acctId.toString()}:${password}`, 'utf8')
-      .digest('hex')
-    let [dl, ul] = ['0', '0']
+    const key = password
     try {
       if (await this.cl.exists(key)) {
         throw new Error('duplicate password.')
       }
       const currentKey = await this.cl.get('user:' + acctId.toString())
       if (currentKey) {
-        const [td, tu] = await this.cl.hmget(currentKey, 'download', 'upload')
-        dl = td || '0'
-        ul = tu || '0'
-        await this.cl.del(currentKey)
+        await this.cl
+          .multi()
+          .eval(
+            "local r = redis.call('hmget', KEYS[1], 'upload', 'download');" +
+              "redis.call('hmset', KEYS[2], 'upload', r[1], 'download', r[2]);",
+            2,
+            currentKey,
+            key,
+          )
+          .del(currentKey)
+          .set('user:' + acctId.toString(), key)
+          .exec()
+      } else {
+        await this.cl
+          .multi()
+          .set('user:' + acctId.toString(), key)
+          .hmset(key, { download: '0', upload: '0' })
+          .exec()
       }
-      await this.cl.set('user:' + acctId.toString(), key)
-      await this.cl.hmset(key, { download: dl, upload: ul })
       logger.debug(`Added user: ${acctId}`)
       return { type: ECommand.Add, id: acctId }
     } catch (e) {
@@ -65,9 +76,14 @@ class RedisClient extends DBClient {
     try {
       const currentKey = await this.cl.get('user:' + acctId.toString())
       if (currentKey) {
-        await this.cl.del(currentKey)
+        await this.cl
+          .multi()
+          .del(currentKey)
+          .del('user:' + acctId.toString())
+          .exec()
+      } else {
+        await this.cl.del('user:' + acctId.toString())
       }
-      await this.cl.del('user:' + acctId.toString())
       logger.debug(`Removed user: ${acctId}`)
       return { type: ECommand.Delete, id: acctId }
     } catch (e) {
@@ -78,24 +94,28 @@ class RedisClient extends DBClient {
   public getFlow = async (): Promise<FlowResult> => {
     try {
       const accounts = await this.cl.keys('user:*')
-      const result = await Promise.all(
-        accounts.map(async (user: string) => {
-          const key = await this.cl.get(user)
-          let flow = 0
-          if (key) {
-            const [dl, ul] = await this.cl.hmget(key, 'download', 'upload')
-            await this.cl.hincrby(key, 'download', -parseInt(dl || '0', 10))
-            await this.cl.hincrby(key, 'upload', -parseInt(ul || '0', 10))
-            flow =
-              (dl ? parseInt(dl, 10) || 0 : 0) +
-              (ul ? parseInt(ul, 10) || 0 : 0)
-          }
-          return {
-            id: parseInt(user.slice(5), 10),
-            flow: flow,
-          }
-        }),
-      )
+      const result = (
+        await Promise.all(
+          accounts.map(async (user: string) => {
+            const key = await this.cl.get(user)
+            let flow = 0
+            if (key) {
+              const [dl, ul]: string[] = (
+                await this.cl
+                  .multi()
+                  .hmget(key, 'download', 'upload')
+                  .hmset(key, 'download', '0', 'upload', '0')
+                  .exec()
+              )[0][1]
+              flow = parseInt(dl, 10) + parseInt(ul, 10)
+            }
+            return {
+              id: parseInt(user.slice(5), 10),
+              flow: flow,
+            }
+          }),
+        )
+      ).filter((user) => user.flow !== 0 && user.flow !== null)
       logger.debug('Flow: ' + JSON.stringify(result))
       return { type: ECommand.Flow, data: result }
     } catch (e) {
