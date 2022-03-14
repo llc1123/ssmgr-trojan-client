a1f69160617c2bbf5491bf26dd53ac4ff1252253
Roy Li <me@dada.li>
2022-03-14 22:08:55 +0100

    require version from package.json

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,5 @@
-import { version } from '../package.json'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const version = require('../package.json').version
 
 export type Version = typeof version
 export { version }
